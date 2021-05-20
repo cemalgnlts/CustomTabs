16498cd793540bf9403786fb2072148aa00defdd
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2021-05-20 12:43:21 +0300

    Formatted

--- src/com/candlelight/customtabs/plugins/user/CustomTabsManager.java
+++ src/com/candlelight/customtabs/plugins/user/CustomTabsManager.java
@@ -16,13 +16,12 @@ import androidx.browser.customtabs.CustomTabsSession;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CustomTabsManager
-{
-    final String BROWSER_CHROME  = "com.android.chrome";
-	final String BROWSER_SAMSUNG = "com.sec.android.app.sbrowser";
+public class CustomTabsManager {
+    final String BROWSER_CHROME = "com.android.chrome";
+    final String BROWSER_SAMSUNG = "com.sec.android.app.sbrowser";
 
     private String customTabsPackageName;
-	private ArrayList<String> packagesSupportingCustomTabs;
+    private ArrayList<String> packagesSupportingCustomTabs;
 
     // Tabs fields.
     private CustomTabsClient mCustomTabsClient;
@@ -32,17 +31,17 @@ public class CustomTabsManager
     Activity activity;
 
     public CustomTabsManager(Context context) {
-        activity = (Activity)context;
-		packagesSupportingCustomTabs = getCustomTabsPackageList();
+        activity = (Activity) context;
+        packagesSupportingCustomTabs = getCustomTabsPackageList();
         customTabsPackageName = getCustomTabsPackageName(packagesSupportingCustomTabs);
-		
+
         CustomTabsClient.bindCustomTabsService(context, customTabsPackageName, connection);
 
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
         builder.setShowTitle(true);
         builder.enableUrlBarHiding();
-//        builder.setStartAnimations(context, android.R.anim.slide_in_left, android.R.anim.slide_out_right);
-//        builder.setExitAnimations(context, android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        //        builder.setStartAnimations(context, android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        //        builder.setExitAnimations(context, android.R.anim.slide_in_left, android.R.anim.slide_out_right);
 
         customTabsIntent = builder.build();
         customTabsIntent.intent.setPackage(customTabsPackageName);
@@ -74,19 +73,19 @@ public class CustomTabsManager
 
     CustomTabsCallback callback = new CustomTabsCallback() {};
 
-	/**
+    /**
      * Returns a package name that support Custom Tabs.
      */
-	public String getCustomTabsPackageName(ArrayList<String> packages) {
-		// If chrome exists select chrome default.
-        if(packages.contains(BROWSER_CHROME))
+    public String getCustomTabsPackageName(ArrayList<String> packages) {
+        // If chrome exists select chrome default.
+        if (packages.contains(BROWSER_CHROME))
             return BROWSER_CHROME;
-		else if(packages.contains(BROWSER_SAMSUNG))
-			return BROWSER_SAMSUNG;
-		else
-			return packages.get(0);
-	}
-	
+        else if (packages.contains(BROWSER_SAMSUNG))
+            return BROWSER_SAMSUNG;
+        else
+            return packages.get(0);
+    }
+
     /**
      * Returns a packages list that support Custom Tabs.
      */
@@ -98,24 +97,24 @@ public class CustomTabsManager
         // Get all apps that can handle VIEW intents.
         List<ResolveInfo> resolvedActivityList = pm.queryIntentActivities(activityIntent, PackageManager.MATCH_ALL);
         ArrayList<String> listSupportingCustomTabs = new ArrayList<>();
-        for(ResolveInfo info : resolvedActivityList) {
+        for (ResolveInfo info: resolvedActivityList) {
             Intent serviceIntent = new Intent();
             serviceIntent.setAction(CustomTabsService.ACTION_CUSTOM_TABS_CONNECTION);
             serviceIntent.setPackage(info.activityInfo.packageName);
-			
+
             // Check if this package also resolves the Custom Tabs service.
-            if(pm.resolveService(serviceIntent, 0) != null) {
+            if (pm.resolveService(serviceIntent, 0) != null) {
                 listSupportingCustomTabs.add(info.activityInfo.packageName);
             }
         }
-        
+
         return listSupportingCustomTabs;
     }
-	
-	/**
+
+    /**
      * Returns a ArrayList that support Custom Tabs variable.
      */
-	public List<String> getPackagesSupportingCustomTabs() {
-		return packagesSupportingCustomTabs;
-	}
+    public List<String> getPackagesSupportingCustomTabs() {
+        return packagesSupportingCustomTabs;
+    }
 }
